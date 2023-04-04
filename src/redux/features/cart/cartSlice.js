51ab86562f2b90432890f021a019c9995180c703
Luis Zubia <soyzubia@gmail.com>
2023-04-03 21:36:21 -0700

    Import cartItems array && Set initialState cartItems with imported cartItems

--- src/redux/features/cart/cartSlice.js
+++ src/redux/features/cart/cartSlice.js
@@ -1,7 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit';
+import cartItems from '../../../settings/cartItems';
 
 const initialState = {
-  cartItems: [],
+  cartItems,
   amount: 0,
   total: 0,
   isLoading: true,
