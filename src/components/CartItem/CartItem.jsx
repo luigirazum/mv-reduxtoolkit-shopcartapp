fc231ab6991d5b5a1d9205a745621c9da962986d
Luis Zubia <soyzubia@gmail.com>
2023-04-03 22:08:54 -0700

    Add code for CartItem component && Install prop-types package to add propTypes to the component

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,3 +1,40 @@
-const CartItem = () => <div>CartItem</div>;
+import PropTypes from 'prop-types';
+import { ChevronUp, ChevronDown } from '../icons/icons';
+
+const CartItem = ({
+  img, title, price, amount,
+}) => (
+  <article className="cart-item">
+    <img src={img} alt={title} />
+    <div>
+      <h4>{title}</h4>
+      <h4 className="item-price">
+        $
+        {price}
+      </h4>
+      {/* remove button */}
+      <button type="button" className="remove-btn">remove</button>
+    </div>
+    <div>
+      {/* increase amount */}
+      <button type="button" className="amount-btn">
+        <ChevronUp />
+      </button>
+      {/* amount */}
+      <p className="amount">{amount}</p>
+      {/* decrease amount */}
+      <button type="button" className="amount-btn">
+        <ChevronDown />
+      </button>
+    </div>
+  </article>
+);
+
+CartItem.propTypes = {
+  img: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired,
+  amount: PropTypes.number.isRequired,
+};
 
 export default CartItem;
