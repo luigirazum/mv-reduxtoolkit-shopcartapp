3eb8338bc15cce9fb8ff96cf0e3860b1ab2b0a75
Luis Zubia <soyzubia@gmail.com>
2023-04-04 19:33:05 -0700

    Build bundle to deploy

--- src/components/CartContainer/CartContainer.jsx
+++ src/components/CartContainer/CartContainer.jsx
@@ -11,7 +11,7 @@ const CartContainer = () => {
       <section className="cart">
         {/* cart header */}
         <header>
-          <h2>your bag</h2>
+          <h2>your cart</h2>
           <h4 className="empty-cart">is currently empty</h4>
         </header>
       </section>
@@ -21,7 +21,7 @@ const CartContainer = () => {
     <section className="cart">
       {/* cart header */}
       <header>
-        <h2>your bag</h2>
+        <h2>your cart</h2>
       </header>
       {/* cart items */}
       <div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <nav>
       <div className="nav-center">
-        <h3>redux toolkit</h3>
+        <h3>shopCart app</h3>
         <div className="nav-container">
           <CartIcon />
           <div className="amount-container">
